a4cf4d7f493bc55c956dc86295a46b38edf90fc8
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-21 01:25:37 +0200

    feat: added support for database connexions feat: created getServerConfig method to check server settings

--- src/commands/Abstract/AbstractCommand.ts
+++ src/commands/Abstract/AbstractCommand.ts
@@ -1,12 +1,41 @@
-import { PrismaClient } from "@prisma/client";
-import { Command, CommandConfig } from "../../handlers/commands-types";
+import { PrismaClient, Guild } from "@prisma/client";
+import { Message } from "discord.js";
+import { Database } from "../../database";
+import { CommandConfig } from "../../handlers/commands-types";
 
 export abstract class AbstractCommand {
-  protected readonly prisma: PrismaClient = new PrismaClient();
+  protected prisma: PrismaClient;
   protected config!: CommandConfig;
-  constructor() {}
+
+  constructor(database: Database) {
+    this.prisma = database.getDB();
+  }
 
   public getConfig(): CommandConfig {
     return this.config;
+  }
+
+  protected async getServerConfig(message: Message): Promise<Guild | boolean> {
+    try {
+      const guild = await this.prisma.guild.findUnique({
+        where: {
+          guildId: message.guild?.id,
+        },
+      });
+
+      if (!guild) {
+        return false;
+      }
+
+      return guild;
+      // return this.prisma.guildConfig.findUnique({
+      //   where: {
+      //     guildId: guild
+      //   }
+      // })
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
   }
 }
