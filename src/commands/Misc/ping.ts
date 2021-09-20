ef93cc4dfc1483f6a2017a845cc765575a2d9770
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-09-20 17:09:13 +0200

    feat: refactored command to be a class and  inherited from Abstract Command

--- src/commands/Misc/ping.ts
+++ src/commands/Misc/ping.ts
@@ -1,34 +1,42 @@
 import { Message, Permissions } from "discord.js";
-import { Command } from "../../types";
+import { Command, CommandConfig } from "../../handlers/commands-types";
+import { AbstractCommand } from "../Abstract/AbstractCommand";
 
-const run = async (message: Message, args: any) => {
-  message.channel.send("Pong!");
-  message.channel.send("User: " + args["user"]);
-  message.channel.send("Title: " + args["title"]);
-  message.channel.send("Full Text: " + args["content"]);
-};
+class PingCommand extends AbstractCommand {
+  constructor() {
+    super();
+  }
+  private run = async (message: Message, args: any): Promise<void> => {
+    message.channel.send("Pong!");
+    message.channel.send("User: " + args["user"]);
+    message.channel.send("Title: " + args["title"]);
+    message.channel.send("Full Text: " + args["content"]);
+  };
 
-export const config = {
-  name: "ping",
-  usage: `%prefix% ping <message>`,
-  aliases: ["pong"],
-  category: "misc",
-  params: [
-    {
-      name: "user",
-      type: "USER",
-      required: true,
-    },
-    {
-      name: "title",
-      required: true,
-    },
-    {
-      name: "content",
-      type: "FULLTEXT",
-      required: false,
-    },
-  ],
-  permission: [Permissions.FLAGS.MANAGE_ROLES],
-  run,
-} as Command;
+  protected config: CommandConfig = {
+    name: "ping",
+    usage: `%prefix% ping <message>`,
+    aliases: ["pong"],
+    category: "misc",
+    params: [
+      {
+        name: "user",
+        type: "USER",
+        required: true,
+      },
+      {
+        name: "title",
+        required: true,
+      },
+      {
+        name: "content",
+        type: "FULLTEXT",
+        required: false,
+      },
+    ],
+    permission: [Permissions.FLAGS.MANAGE_ROLES],
+    run: this.run,
+  };
+}
+
+module.exports = PingCommand;
