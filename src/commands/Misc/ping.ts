454c179dc07651b2e445f2e9b872a64030895c78
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-09 08:57:22 +0200

    feat: set args type to be RunCommand

--- src/commands/Misc/ping.ts
+++ src/commands/Misc/ping.ts
@@ -1,17 +1,21 @@
 import { Message, Permissions } from "discord.js";
 import { Database } from "$utils/database";
 import { AbstractCommand } from "$abstract/AbstractCommand";
-import type { CommandConfig } from "$types/commands";
+import type { CommandConfig, RunCommand } from "$types/commands";
+import type { Argument } from "$types/arguments";
 
 export class PingCommand extends AbstractCommand {
   constructor(database: Database) {
     super(database);
   }
-  private run = async (message: Message, args: any): Promise<void> => {
-    message.channel.send("Pong!");
-    message.channel.send("User: " + args["user"]);
-    message.channel.send("Title: " + args["title"]);
-    message.channel.send("Full Text: " + args["content"]);
+  private run = async ({
+    message,
+    args = {},
+    translation,
+  }: RunCommand): Promise<void> => {
+    translation;
+    message;
+    args;
   };
 
   protected config: CommandConfig = {
@@ -21,20 +25,12 @@ export class PingCommand extends AbstractCommand {
     category: "misc",
     params: [
       {
-        name: "user",
-        type: "USER",
-        required: true,
-      },
-      {
-        name: "title",
-        required: true,
-      },
-      {
-        name: "content",
+        name: "reason",
         type: "FULLTEXT",
-        required: false,
+        required: true,
+        generic: true,
       },
-    ],
+    ] as Argument[],
     permission: [Permissions.FLAGS.MANAGE_ROLES],
     run: this.run,
   };
